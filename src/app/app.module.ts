07a8cf120140966ccbc0dc35e0b1b0ff32516505
IvanSaienko <ivansaienkowork@gmail.com>
2023-06-09 13:45:04 +0300

    Added base structure and added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogResultComponent } from './components/dialog-result/dialog-result.component';
+import { GameComponent } from './components/game/game.component';
+import { ScoreComponent } from './components/score/score.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogResultComponent,
+    GameComponent,
+    ScoreComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
