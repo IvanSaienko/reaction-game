a274a80d873f98de04ee4373f259d463032025d5
IvanSaienko <ivansaienkowork@gmail.com>
2023-06-13 13:16:35 +0300

    Added csore and dialog with result of the game

--- src/app/components/dialog-result/dialog-result.component.ts
+++ src/app/components/dialog-result/dialog-result.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CELL_STATUS } from 'src/app/types/cell';
+import { GameScore } from 'src/app/types/game-score';
 
 @Component({
   selector: 'app-dialog-result',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-result.component.scss']
 })
 export class DialogResultComponent {
+  public score: GameScore;
+  public cellStatuses = CELL_STATUS;
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {score: GameScore}, private dialogRef: MatDialogRef<DialogResultComponent>) {
+    this.score = data.score;
+  }
+
+  public startNewGame(): void {
+    this.dialogRef.close(true);
+  }
+
+  public getHeader(): string {
+    return this.score.userCount > this.score.pcCount ? 'Ви перемогли!' : 'Компьютер переміг!';
+  }
+
+  public getWinner(): CELL_STATUS {
+    return this.score.userCount > this.score.pcCount ? CELL_STATUS.USER : CELL_STATUS.PC;
+  }
 }
--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { DEFAULT_REACTION_TIME, GameService } from 'src/app/services/game.service';
 import { CELL_STATUS, CellData } from 'src/app/types/cell';
 import { GameOptions } from 'src/app/types/game-options';
+import { DialogResultComponent } from '../dialog-result/dialog-result.component';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit, OnDestroy {
+export class GameComponent implements OnInit, AfterContentChecked, OnDestroy {
 
   public CELL_STATUS = CELL_STATUS;
   public gameMap: CellData[][];
   public isGameReady = true;
+  
   private reactionTime?: number;
   private timeInputSubscription?: Subscription;
   private gameEndSubscription?: Subscription;
+  private dialogSubscription?: Subscription;
+  private dialogRef?: MatDialogRef<DialogResultComponent>;
 
-  constructor(public gameService: GameService) {
+  constructor(public gameService: GameService, private dialog: MatDialog) {
     this.gameMap = gameService.gameMap;
   }
 
@@ -28,7 +33,17 @@ export class GameComponent implements OnInit, OnDestroy {
       this.reactionTime = time;
     });
     this.gameEndSubscription = this.gameService.isFinish$.subscribe(() => {
-      console.log('Game ended');
+      this.dialogRef = this.dialog.open(DialogResultComponent, {
+        data: {  score: this.gameService.gameScore },
+      });
+    });
+  }
+
+  ngAfterContentChecked(): void {
+    this.dialogSubscription = this.dialogRef?.afterClosed().subscribe((isNewGame: boolean) => {
+      if (isNewGame) {
+        this.startNewGame();
+      }
     });
   }
 
@@ -42,5 +57,6 @@ export class GameComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.timeInputSubscription?.unsubscribe();
     this.gameEndSubscription?.unsubscribe();
+    this.dialogSubscription?.unsubscribe();
   }
 }
